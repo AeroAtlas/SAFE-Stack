d5bd6dfc8d606e92d4628dcf666d074e7fee5b7c
Andrew Hardemon <aeroatlas@ymail.com>
2020-08-05 23:04:37 -0700

    working with fable. will implement elm later

---
+++ src/App.fs
@@ -0,0 +1,13 @@
+module App
+
+open Browser.Dom
+
+let minus = document.getElementById "minus"
+let plus = document.getElementById "plus"
+
+let mutable count = 0
+
+let textcount = document.getElementById "textcount"
+textcount.innerHTML <- (string count)
+
+// minus.addEventListener("click", fun _ -> count <- count -1 textcount.innerHTML <- (string count) )
